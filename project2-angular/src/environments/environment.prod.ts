a20b280ac2ce34bd9d0d1eb8dd18e3450c5a68a8
ericmiller <emiller@skillstorm.com>
2021-10-25 17:13:21 -0500

    Minor bug fix in the plan list
    Bug fix. The reference to user has been changed to plan.

--- project2-angular/src/environments/environment.prod.ts
+++ project2-angular/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   userAPIurl : 'https://azuretraining-em-10202021.azurewebsites.net/api/Users',
-  deviceAPIurl: 'https://azuretraining-em-10202021.azurewebsites.net/api/Devices'
+  deviceAPIurl: 'https://azuretraining-em-10202021.azurewebsites.net/api/Devices',
+  planAPIurl: 'https://azuretraining-em-10202021.azurewebsites.net/api/Plan'
 };
