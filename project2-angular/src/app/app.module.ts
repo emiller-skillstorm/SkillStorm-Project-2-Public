6c9df67f6ed18364d644574b92eebdfb6ebda218
ericmiller <emiller@skillstorm.com>
2021-10-25 09:29:37 -0500

    Removed duplicates, fixed imports, minor HTML fix

--- project2-angular/src/app/app.module.ts
+++ project2-angular/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
+import { UsersComponent } from './usersTest/users.component';
 
 @NgModule({
   declarations: [
